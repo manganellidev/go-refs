7b300ddda6e04d49c2309ff97ac899afa00acb4f
manganellidev <manganellidev@gmail.com>
2024-07-15 18:11:02 -0300

    call all functions in main

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"main/foundation"
+)
+
+func main () {
+	foundation.HelloWorld()
+	foundation.VariablesConstants()
+	foundation.DataTypes()
+	foundation.Operators()
+	foundation.Pointers()
+	foundation.Printf()
+	foundation.Loops()
+	foundation.DecisionMaking()
+	foundation.Arrys()
+}
